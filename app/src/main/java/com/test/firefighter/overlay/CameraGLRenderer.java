317bcb277563eb5c84e6069e31cc641164200823
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2015-09-29 09:05:13 -0400

    Ported to new android project (with errors)
    Copied into a new Android Studio project the java files, xml files and jar files.
    
    This fixed the problem with the run configuration, where you couldn't choose a default Activity. However, there are still errors. For example the missing POIList class and problems resolving "R".

---
+++ app/src/main/java/com/test/firefighter/overlay/CameraGLRenderer.java
@@ -0,0 +1,89 @@
+package com.test.firefighter.overlay;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import com.test.firefighter.POIList;
+import com.test.firefighter.SensorSource;
+import com.test.firefighter.R;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
+import android.opengl.GLU;
+
+import android.opengl.GLSurfaceView.Renderer;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class CameraGLRenderer implements Renderer {
+    private Object lock;
+    Context context;
+    float aspect;
+    private POIList POIs;
+    Location userLocation;
+    private SensorSource mSensorSource;
+
+    // Constructor with global application context
+    public CameraGLRenderer(Context context, POIList POIs, SensorSource mSensorSource) {
+        this.context = context;
+        this.POIs = POIs;
+        this.lock = new Object();
+
+        this.mSensorSource = mSensorSource;
+        LocalBroadcastManager.getInstance(context).registerReceiver(locationBroadcastReceiver,
+                new IntentFilter(context.getString(R.string.LocationEvent)));
+    }
+
+    /* Called when the surface is first created or re-created */
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set color's clear-value to
+        // black
+        gl.glClearDepthf(1.0f); // Set depth's clear-value to farthest
+        gl.glEnable(GL10.GL_DEPTH_TEST); // Enables depth-buffer for hidden
+        // surface removal
+        gl.glDepthFunc(GL10.GL_LEQUAL); // The type of depth testing to do
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST); // nice
+        // perspective
+        // view
+        gl.glShadeModel(GL10.GL_SMOOTH); // Enable smooth shading of color
+        gl.glDisable(GL10.GL_DITHER); // Disable dithering for better performance
+    }
+
+    /* Called after onSurfaceCreated() or whenever the window's size changes */
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        if (height == 0)
+            height = 1; // To prevent divide by zero
+        aspect = (float) width / height;
+
+        gl.glViewport(0, 0, width, height); //Set the viewport (display area) to cover the entire window
+
+        // Setup perspective projection, with aspect ratio matches viewport
+        gl.glMatrixMode(GL10.GL_PROJECTION); // Select projection matrix
+        gl.glLoadIdentity(); // Reset projection matrix
+        GLU.gluPerspective(gl, 45, aspect, 0.1f, 100.f); //Use perspective projection
+
+        gl.glMatrixMode(GL10.GL_MODELVIEW); // Select model-view matrix
+        gl.glLoadIdentity(); // Reset
+    }
+
+    public void onDrawFrame(GL10 gl) {
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT); //Clear color and depth buffers using clear-value set earlier
+
+        synchronized(lock) {
+            userLocation = mSensorSource.getLocation();
+            gl.glLoadIdentity();
+            POIs.render(gl, userLocation);
+        }
+    }
+
+    /* Receiver for Location broadcast events  */
+    private BroadcastReceiver locationBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            userLocation = mSensorSource.getLocation();
+        }
+    };
+
+}
---
+++ app/src/main/java/com/test/firefighter/overlay/OverlayController.java
@@ -0,0 +1,78 @@
+package com.test.firefighter.overlay;
+
+import ca.nehil.rter.streamingapp.POIList;
+import ca.nehil.rter.streamingapp.SensorSource;
+import android.content.Context;
+
+/**
+ * NORTH: 0 deg
+ * EAST: +90 deg
+ * WEST: -90 deg
+ * SOUTH: +/- 180 deg
+ *
+ * @author stepan
+ */
+
+public class OverlayController {
+    protected float desiredOrientation;
+    protected float currentOrientation;
+
+    protected CameraGLSurfaceView mGLView;
+    protected CameraGLRenderer mGLRenderer;
+    protected Context context;
+
+    float declination = 0;	//geo magnetic declinationf from true North
+
+    public float orientationTolerance = 10.0f; // Max orientation tolerance in degrees
+
+    public OverlayController(Context context, POIList POIs, SensorSource mSensorSource) {
+        this.context = context;
+        this.mGLView = new CameraGLSurfaceView(context, POIs, mSensorSource);
+        this.mGLRenderer = this.mGLView.getGLRenderer();
+    }
+
+    /**
+     * @return the camera GLView
+     */
+    public CameraGLSurfaceView getGLView() {
+        return this.mGLView;
+    }
+
+    /**
+     * Set the desired absolute bearing Should be between +180 and -180, but
+     * will work otherwise
+     *
+     * @param orientation
+     */
+    public void setDesiredOrientation(float orientation) {
+        orientation = fixAngle(orientation);
+        desiredOrientation = orientation;
+    }
+
+    /**
+     * makes sure angle is between -180 and 180
+     *
+     * @param angle
+     * @return fixed angle
+     */
+    protected float fixAngle(float angle) {
+        if (angle > 180.0f) {
+            angle = -180.0f + angle % 180;
+        } else if (angle < -180.0f) {
+            angle = 180.0f - Math.abs(angle) % 180;
+        }
+
+        return angle;
+    }
+
+    /**
+     * Set the desired offset from the current bearing should be between +180
+     * and -180, but will work otherwise
+     *
+     * @param offset
+     */
+    public void setOrientationOffset(float offset) {
+        this.setDesiredOrientation(currentOrientation + offset);
+    }
+
+}
